f26c85e3b748d65ea0a89a11f47b45c668835d1f
James Orlowski <jamesorlowski.1@gmail.com>
2021-01-17 10:34:07 -0500

    Initial Commit

---
+++ GM-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GM-Assignment
+//
+//  Created by James Orlowski on 1/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
