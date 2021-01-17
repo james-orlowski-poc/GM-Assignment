9e13ccdedc44b338d61003f316514cc12666269a
James Orlowski <jamesorlowski.1@gmail.com>
2021-01-17 11:31:41 -0500

    Added to the APIModel to support the author name + commit message. They are also being printed in the console

--- GM-Assignment/API/APIModel.swift
+++ GM-Assignment/API/APIModel.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 struct CommitHistoryData: Decodable {
+    struct Commit: Decodable {
+
+        struct Author: Decodable {
+            let name: String
+        }
+
+        let message: String
+        let author: Author
+    }
+    
+    let commit: Commit
     let sha: String
 }
