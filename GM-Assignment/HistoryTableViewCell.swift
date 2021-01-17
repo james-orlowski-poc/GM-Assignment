9e7b40dc8a2d0c992f48e9fa6de6408106ab4309
James Orlowski <jamesorlowski.1@gmail.com>
2021-01-17 11:01:56 -0500

    Added a custom cell with three labels for: author, commit message, and the hash value Hard coded the numberOfRowsInSection delegate to display a few cells

---
+++ GM-Assignment/HistoryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  HistoryTableViewCell.swift
+//  GM-Assignment
+//
+//  Created by James Orlowski on 1/17/21.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    
+    static let identifier = "HistoryTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
