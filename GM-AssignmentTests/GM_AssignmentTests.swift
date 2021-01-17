b7924fa83ba33da88ce0e4a30a97d7a542d6ab8d
James Orlowski <jamesorlowski.1@gmail.com>
2021-01-17 11:49:03 -0500

    Added additional network testing: invalid URL + various status codes

--- GM-AssignmentTests/GM_AssignmentTests.swift
+++ GM-AssignmentTests/GM_AssignmentTests.swift
@@ -12,12 +12,31 @@ class GM_AssignmentTests: XCTestCase {
 
     func testAPINetworkCall() throws {
         let expectation = self.expectation(description: "")
-        expectation.expectedFulfillmentCount = 1
+        expectation.expectedFulfillmentCount = 4
         
         // Standard use case. Make sure that the API is returning data + the status code is 200.
         APIUtility.fetchHistoryDataFromAPI(urlString: APIUtility.urlString) { (data: Data?, statusCode: Int?) in
             XCTAssertNotNil(data)
             XCTAssertTrue(statusCode == 200)
+            expectation.fulfill()
+        }
+        
+        // Test an invalid URL
+        APIUtility.fetchHistoryDataFromAPI(urlString: "https://invalid url!!!") { (data: Data?, statusCode: Int?) in
+            XCTAssertNil(data)
+            XCTAssertNil(statusCode)
+            expectation.fulfill()
+        }
+        
+        APIUtility.fetchHistoryDataFromAPI(urlString: "https://httpstat.us/200") { (data: Data?, statusCode: Int?) in
+            XCTAssertNotNil(data)
+            XCTAssertTrue(statusCode == 200)
+            expectation.fulfill()
+        }
+        
+        APIUtility.fetchHistoryDataFromAPI(urlString: "https://httpstat.us/403") { (data: Data?, statusCode: Int?) in
+            XCTAssertNotNil(data)
+            XCTAssertTrue(statusCode == 403)
             expectation.fulfill()
         }
         
