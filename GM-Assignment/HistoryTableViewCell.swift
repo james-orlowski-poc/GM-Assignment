352b607f5119fac818d503b0d151dd67ce7d8f88
James Orlowski <jamesorlowski.1@gmail.com>
2021-01-17 11:10:42 -0500

    Added a function to configure the cell. The function is taking in strings that will set the label text, just using the indexPath.row value to make it dynamic. The labels in the custom cell now have dynamic number of lines, this will allow any and all text to be displayed on the label.

--- GM-Assignment/HistoryTableViewCell.swift
+++ GM-Assignment/HistoryTableViewCell.swift
@@ -10,16 +10,15 @@ import UIKit
 class HistoryTableViewCell: UITableViewCell {
     
     static let identifier = "HistoryTableViewCell"
+    
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var hashLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configure(authorString: String, messageString: String, hashString: String) {
+        authorLabel.text = authorString
+        messageLabel.text = messageString
+        hashLabel.text = hashString
     }
 
 }
