7f11ae839ea8382c06fdb87882082187b9aaa287
James Orlowski <jamesorlowski.1@gmail.com>
2021-01-17 11:45:43 -0500

    Added an initial unit test for the API network request

--- GM-AssignmentTests/GM_AssignmentTests.swift
+++ GM-AssignmentTests/GM_AssignmentTests.swift
@@ -10,24 +10,17 @@ import XCTest
 
 class GM_AssignmentTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAPINetworkCall() throws {
+        let expectation = self.expectation(description: "")
+        expectation.expectedFulfillmentCount = 1
+        
+        // Standard use case. Make sure that the API is returning data + the status code is 200.
+        APIUtility.fetchHistoryDataFromAPI(urlString: APIUtility.urlString) { (data: Data?, statusCode: Int?) in
+            XCTAssertNotNil(data)
+            XCTAssertTrue(statusCode == 200)
+            expectation.fulfill()
         }
+        
+        self.waitForExpectations(timeout: 20)
     }
-
 }
